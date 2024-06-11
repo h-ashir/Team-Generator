c100a15d36564eefbc579b349cffc21126f331be
h-ashir <hashiraslam2015@gmail.com>
2024-06-11 17:25:05 +0530

    Confirm login and signup working

--- js/signup.js
+++ js/signup.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
- import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+ import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
  
  const firebaseConfig = {
     apiKey: "AIzaSyAIuCN5NMapt-HyvTWmEXPOhXTdBYlVhOk",
     authDomain: "login-backend-59af8.firebaseapp.com",
+    databaseURL: "https://login-backend-59af8-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "login-backend-59af8",
     storageBucket: "login-backend-59af8.appspot.com",
     messagingSenderId: "1097943042656",
     appId: "1:1097943042656:web:174161d4af1d7017cad105",
     measurementId: "G-Q9C3E9L2P6"
- };
+  };
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
