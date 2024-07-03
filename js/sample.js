52d454c2ffe6e99c39965dddbf4b2937983a5944
h-ashir <hashiraslam2015@gmail.com>
2024-07-03 13:15:52 +0530

    Add sample.ts in src for typescript

---
+++ js/sample.js
@@ -0,0 +1,29 @@
+"use strict";
+// const subtract = (a:number,b:number) => {
+//     console.log(a-b);
+// }
+// subtract(5,2);
+const display = (name) => {
+    console.log(name);
+};
+display({ id: 100 });
+let numberArr = [1, 2, 3];
+let stringArr = ["hello"];
+console.log(numberArr);
+console.log(stringArr);
+const car = [
+    {
+        type: "Toyota",
+        model: "Innova",
+        year: 2000
+    },
+    {
+        type: "Toyota",
+        model: "camry",
+    },
+];
+console.log(car);
+// use interface for interface
+const sum = (a, b) => {
+    return a + b;
+};
