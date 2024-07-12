894e0c727aa51ff4900025909713adba672c956e
GOKULTHEGREAT <gokul19072001@gmail.com>
2024-07-12 10:26:46 +0530

    retrieving username from firebase

--- js/team-generation-implementation.js
+++ js/team-generation-implementation.js
@@ -1,15 +1,6 @@
-// Attach event listener to the range input
-// const slider = document.getElementById('customRange3');
-// const sliderValue = document.getElementById('sliderValue');
-
-// slider.addEventListener('input', function() {
-//   sliderValue.textContent = this.value + '%';
-// });
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyAIuCN5NMapt-HyvTWmEXPOhXTdBYlVhOk",
   authDomain: "login-backend-59af8.firebaseapp.com",
@@ -20,43 +11,47 @@ const firebaseConfig = {
   appId: "1:1097943042656:web:174161d4af1d7017cad105",
   measurementId: "G-Q9C3E9L2P6"
 };
-
+ 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-const loginButton = document.getElementById('loginButton');
-const logoutButton = document.getElementById('logoutButton');
-const historyButton = document.getElementById('historyButton');
-const slider = document.getElementById('customRange3');
-const sliderValue = document.getElementById('sliderValue');
-const generateTeamsButton = document.querySelector('.generate-area button');
-
+ 
+// const loginButton = document.getElementById('loginButton');
+// const logoutButton = document.getElementById('logoutButton');
+// const historyButton = document.getElementById('historyButton');
+// const dropdownButton = document.getElementById('dropdownButton');
+// const slider = document.getElementById('customRange3');
+// const sliderValue = document.getElementById('sliderValue');
+// const generateTeamsButton = document.querySelector('.generate-area button');
+ 
 // Disable 'Generate Teams' button initially
 // generateTeamsButton.disabled = true;
 // generateTeamsButton.style.opacity = 0.5;
-
+ 
 function updateAuthButton(isSignedIn) {
   const loginButton = document.getElementById('loginButton');
   const logoutButton = document.getElementById('logoutButton');
   const historyButton = document.getElementById('historyButton');
-
-  if (loginButton && logoutButton && historyButton) {
+  const dropdownButton = document.getElementById('dropdownButton');
+ 
+  if (loginButton && logoutButton && historyButton && dropdownButton) {
       if (isSignedIn) {
           historyButton.style.display = "block";
           logoutButton.style.display = "block";
           loginButton.style.display = "none";
+          dropdownButton.style.display = "block";
       } else {
           historyButton.style.display = "none";
           logoutButton.style.display = "none";
           loginButton.style.display = "block";
+          dropdownButton.style.display = "none";
       }
   }
 }
-
+ 
 onAuthStateChanged(auth, user => {
   updateAuthButton(!!user);
 });
-
+ 
 updateAuthButton(false);
 
 //Pie chart
